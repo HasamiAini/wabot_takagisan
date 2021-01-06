838f7a70a1d77bd632c56f0e369def4c17494248
Takagisan <73755103+HasamiAini@users.noreply.github.com>
2021-01-06 15:18:53 +0700

    Update msgHndlr.js

--- whatsapp bot takagisan/whatsapp bot takagisan/msgHndlr.js
+++ whatsapp bot takagisan/whatsapp bot takagisan/msgHndlr.js
@@ -446,6 +446,24 @@ module.exports = msgHandler = async (client, message) => {
             const quoteanimeKey = quoteanimeJson[quoteanimeIndex]
             client.reply(from, `➸ *Quotes* : ${quoteanimeKey.quote}\n➸ *Karakter* : ${quoteanimeKey.by}`, id)
             break 
+      case 'kpop':
+        if (!isGroupMsg) return client.reply(from, 'Onichan Gomenasai harus di group desu!', id)
+            if (args.length == 0) return client.reply(from, `Untuk menggunakan ${prefix}kpop\nSilahkan ketik: ${prefix}kpop [query]\nContoh: ${prefix}kpop bts\n\nquery yang tersedia:\nblackpink, exo, bts, twice`, id)
+            if (args[0] == 'blackpink' || args[0] == 'exo' || args[0] == 'bts' || args[0] == 'twice') {
+                fetch('https://raw.githubusercontent.com/HasamiAini/Bot_Takagisan/main/' + args[0] + '.txt')
+                .then(res => res.text())
+                .then(body => {
+                    let randomkpop = body.split('\n')
+                    let randomkpopx = randomkpop[Math.floor(Math.random() * randomkpop.length)]
+                    client.sendFileFromUrl(from, randomkpopx, '', '*chwihada*\n*BY:Bot_Takagisan Vers.4.2*\nSupport Bot..*Owner Bot:08319173552*\n*!donasi*', id)
+                })
+                .catch(() => {
+                    client.reply(from, '*Gomenasai Onichan Ada yang error!*', id)
+                })
+            } else {
+                client.reply(from, `Gomenasai Onichan Bukan seperti itu ${prefix}kpop untuk melihat list Perintah`)
+            }
+            break           
      case '!pokemon':
      if (!isGroupMsg) return client.reply(from, 'Onichan Gomenasai harus di group desu!', id)
             q7 = Math.floor(Math.random() * 890) + 1;
